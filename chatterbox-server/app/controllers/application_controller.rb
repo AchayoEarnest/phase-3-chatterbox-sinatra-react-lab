10be85809fbec1b944a54bee742f6b45529ce7c0
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-09-02 07:25:38 +0300

    Done.

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,28 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+
+  get '/messages' do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+
+  post '/messages' do
+    message = Message.create(params)
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(params)
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+    "Message deleted successfully"
+  end
   
 end
